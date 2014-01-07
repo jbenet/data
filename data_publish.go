126f1513a704fb61a413124e60532b23dd025d2b
Juan Batiz-Benet <juan@benet.ai>
2014-01-06 16:15:04 -0800

    go-fmt -- need a pre-commit hook

--- data_publish.go
+++ data_publish.go
@@ -1,14 +1,14 @@
 package data
 
 import (
-  "fmt"
-  "github.com/jbenet/commander"
+	"fmt"
+	"github.com/jbenet/commander"
 )
 
 var cmd_data_publish = &commander.Command{
-  UsageLine: "publish",
-  Short:     "Guided dataset publishing.",
-  Long: `data publish - Guided dataset publishing.
+	UsageLine: "publish",
+	Short:     "Guided dataset publishing.",
+	Long: `data publish - Guided dataset publishing.
 
     This command guides the user through the necessary steps to
     create a data package (Datafile and Manifest), uploads it,
@@ -16,25 +16,25 @@ var cmd_data_publish = &commander.Command{
 
     See 'data pack'.
   `,
-  Run: publishCmd,
+	Run: publishCmd,
 }
 
 func publishCmd(c *commander.Command, args []string) error {
 
-  pOut("==> Guided Data Package Publishing.\n")
+	pOut("==> Guided Data Package Publishing.\n")
 
-  pOut("\n==> Step 1/3: Creating the package.\n")
-  err := packMakeCmd(c, []string{})
-  if err != nil {
-    return err
-  }
+	pOut("\n==> Step 1/3: Creating the package.\n")
+	err := packMakeCmd(c, []string{})
+	if err != nil {
+		return err
+	}
 
-  pOut("\n==> Step 2/3: Uploading the package contents.\n")
-  err = packUploadCmd(c, []string{})
-  if err != nil {
-    return err
-  }
+	pOut("\n==> Step 2/3: Uploading the package contents.\n")
+	err = packUploadCmd(c, []string{})
+	if err != nil {
+		return err
+	}
 
-  pOut("\n==> Step 3/3: Publishing the package to the index.\n")
-  return fmt.Errorf("publishing not implemented yet")
+	pOut("\n==> Step 3/3: Publishing the package to the index.\n")
+	return fmt.Errorf("publishing not implemented yet")
 }
