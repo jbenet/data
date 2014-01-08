adeeae1817ebae82542e7d4da650fba47eed67da
Juan Batiz-Benet <juan@benet.ai>
2014-01-08 05:47:01 -0800

    added email regexp

--- regexp.go
+++ regexp.go
@@ -7,16 +7,19 @@ import (
 var UserRegexp *regexp.Regexp
 var NameRegexp *regexp.Regexp
 var PathRegexp *regexp.Regexp
+var EmailRegexp *regexp.Regexp
 var HandleRegexp *regexp.Regexp
 
 func init() {
 	identRE := "[A-Za-z0-9-_.]+"
 	pathRE := "((" + identRE + ")/(" + identRE + "))"
 	handleRE := "^" + pathRE + "(\\." + identRE + ")?(@" + identRE + ")?$"
+	emailRE := `(?i)[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}`
 
 	UserRegexp = compileRegexp(identRE)
 	NameRegexp = compileRegexp(identRE)
 	PathRegexp = compileRegexp(pathRE)
+	EmailRegexp = compileRegexp(emailRE)
 	HandleRegexp = compileRegexp(handleRE)
 }
 
